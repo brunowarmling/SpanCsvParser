3890921687fce909081ab5b0d50d8aa66dcb874e
Bruno Warmling <bruno.bwg@hotmail.com>
2025-01-17 16:40:10 -0300

    Incluído mais benchmarks

--- CsvParserBenchmark.cs
+++ CsvParserBenchmark.cs
@@ -6,12 +6,19 @@ using FluentResults;
 [MemoryDiagnoser]
 public class CsvParserBenchmark
 {
-    private const string FileName = "Resources\\urs_teste.csv";
+    private const string Folder = "Resources";
+    private const string FileName1Linha = "1_linha.csv";
+    private const string FileName50Linhas = "50_linhas.csv";
+    private const string FileName500Linhas = "500_linhas.csv";
+    private const string FileName5000Linhas = "5000_linhas.csv";
+
+    [Params(FileName1Linha, FileName50Linhas, FileName500Linhas, FileName5000Linhas)]
+    public static string FileName { get; set; }
 
     [Benchmark(Baseline = true)]
     public async ValueTask<Result<List<Ur>>> ParseAsync()
     {
-        await using Stream fileStream = File.OpenRead(FileName);
+        await using Stream fileStream = File.OpenRead(Folder + "\\" + FileName);
         fileStream.Position = 0;
         return await CsvParser.Parsers.CsvParser.ParseAsync(fileStream, CancellationToken.None);
     }
@@ -19,18 +26,15 @@ public class CsvParserBenchmark
     [Benchmark]
     public async ValueTask<Result<(UrNew[], int)>> SpanCsvParser()
     {
-        await using Stream fileStream = File.OpenRead(FileName);
+        await using Stream fileStream = File.OpenRead(Folder + "\\" + FileName);
         fileStream.Position = 0;
         return await SpanCsvParser<UrNew>.ParseAsync(fileStream, AccessStageCsvHelper.PreencherCamposV1, AccessStageCsvHelper.QuantidadeColunasV1, CancellationToken.None);
     }
     [Benchmark]
     public async ValueTask<Result<Ur[]>> CsvHelper()
     {
-        await using Stream fileStream = File.OpenRead(FileName);
-        fileStream.Position = 0;
-
         var config = CsvConfiguration.FromAttributes<Ur>();
-        using var reader = new StreamReader(FileName);
+        using var reader = new StreamReader(Folder + "\\" + FileName);
         using var csv = new CsvReader(reader, config);
         return csv.GetRecords<Ur>().ToArray();
     }
